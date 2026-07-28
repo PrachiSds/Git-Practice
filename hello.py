8e9b88c8f03f22d7ee3eba04ee39715bc6502d71
Prachi Sharma <prachids2025@gmail.com>
2026-07-27 23:47:31 -0500

    Completed Python practice Day 2

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-print("hello git")
-print("first git file learning")
+#print("hello git")
+#print("first git file learning")
+
+def calculate_area(length, width):
+    return length * width
+
+print(calculate_area(50,100))
